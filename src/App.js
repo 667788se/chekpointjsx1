6960607552fb2d4a05aa7c51f9f3f9b4b4eb18fc
Your Name <sehamerhoma@gmail.com>
2021-11-24 16:11:57 +0200

    Adding Project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import hapy from './hapy.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{border: 'solid 1px black', maxWidth: '100vw'}}>
+          <h1 className="title red">HappyAsfor</h1>
+          <br />
+              </div>
+            
+          <img src="/hape.jpg"/>
+          <br />
+          <img src={hapy} alt="hape" />
+          <br />
+          <video width={320} height={240} controls>
+          <source src="myVideo.mp4" type="video/mp4" />
+        </video>
     </div>
   );
 }
